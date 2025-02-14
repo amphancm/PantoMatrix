65c6e0a76c5937058c986d0130e0ba9d2f760638
Adun Nantakaew <amphancm@gmail.com>
2025-02-14 13:34:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
-import spaces
 import os
+import torch
+import spaces
+import librosa
+import numpy as np
+import gradio as gr
+import soundfile as sf
+import OpenGL.GL as gl
+import torch.nn.functional as F
+
 # os.system("Xvfb :99 -ac &")
 # os.environ["DISPLAY"] = ":99"
-import OpenGL.GL as gl
 os.environ["PYOPENGL_PLATFORM"] = "egl"
 os.environ["MESA_GL_VERSION_OVERRIDE"] = "4.1"
-import gradio as gr
-import torch
-import numpy as np
-import soundfile as sf
-import librosa
+
 from torchvision.io import write_video
-from emage_utils.motion_io import beat_format_save
 from emage_utils import fast_render
 from emage_utils.npz2pose import render2d
+from emage_utils.motion_io import beat_format_save
 from models.camn_audio import CamnAudioModel
 from models.disco_audio import DiscoAudioModel
 from models.emage_audio import EmageAudioModel, EmageVQVAEConv, EmageVAEConv, EmageVQModel
-import torch.nn.functional as F
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device      = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 save_folder = "./gradio_results"
 os.makedirs(save_folder, exist_ok=True)
 print(device)
@@ -28,13 +30,13 @@ if not os.path.exists("./emage_evaltools/smplx_models"):
     import subprocess
     subprocess.run(["git", "clone", "https://huggingface.co/H-Liu1997/emage_evaltools"])
 
-model_camn = CamnAudioModel.from_pretrained("H-Liu1997/camn_audio").to(device).eval()
+model_camn  = CamnAudioModel.from_pretrained("H-Liu1997/camn_audio").to(device).eval()
 model_disco = DiscoAudioModel.from_pretrained("H-Liu1997/disco_audio").to(device).eval()
 
-face_motion_vq = EmageVQVAEConv.from_pretrained("H-Liu1997/emage_audio", subfolder="emage_vq/face").to(device).eval()
-upper_motion_vq = EmageVQVAEConv.from_pretrained("H-Liu1997/emage_audio", subfolder="emage_vq/upper").to(device).eval()
-lower_motion_vq = EmageVQVAEConv.from_pretrained("H-Liu1997/emage_audio", subfolder="emage_vq/lower").to(device).eval()
-hands_motion_vq = EmageVQVAEConv.from_pretrained("H-Liu1997/emage_audio", subfolder="emage_vq/hands").to(device).eval()
+face_motion_vq   = EmageVQVAEConv.from_pretrained("H-Liu1997/emage_audio", subfolder="emage_vq/face").to(device).eval()
+upper_motion_vq  = EmageVQVAEConv.from_pretrained("H-Liu1997/emage_audio", subfolder="emage_vq/upper").to(device).eval()
+lower_motion_vq  = EmageVQVAEConv.from_pretrained("H-Liu1997/emage_audio", subfolder="emage_vq/lower").to(device).eval()
+hands_motion_vq  = EmageVQVAEConv.from_pretrained("H-Liu1997/emage_audio", subfolder="emage_vq/hands").to(device).eval()
 global_motion_ae = EmageVAEConv.from_pretrained("H-Liu1997/emage_audio", subfolder="emage_vq/global").to(device).eval()
 
 emage_vq_model = EmageVQModel(
@@ -51,7 +53,7 @@ model_emage = EmageAudioModel.from_pretrained("H-Liu1997/emage_audio").to(device
 def inference_camn(audio_path, sr_model, pose_fps, seed_frames):
     audio_loaded, _ = librosa.load(audio_path, sr=sr_model)
     audio_t = torch.from_numpy(audio_loaded).float().unsqueeze(0).to(device)
-    sid = torch.zeros(1, 1).long().to(device)
+    sid     = torch.zeros(1, 1).long().to(device)
     with torch.no_grad():
         motion_pred = model_camn(audio_t, sid, seed_frames=seed_frames)["motion_axis_angle"]
     t = motion_pred.shape[1]
@@ -77,13 +79,13 @@ def inference_emage(audio_path, sr_model, pose_fps):
     audio_t = torch.from_numpy(audio_loaded).float().unsqueeze(0).to(device)
     sid = torch.zeros(1, 1).long().to(device)
     with torch.no_grad():
-        latent_dict = model_emage.inference(audio_t, sid, emage_vq_model, masked_motion=None, mask=None)
-        face_latent = latent_dict["rec_face"] if model_emage.cfg.lf > 0 and model_emage.cfg.cf == 0 else None
+        latent_dict  = model_emage.inference(audio_t, sid, emage_vq_model, masked_motion=None, mask=None)
+        face_latent  = latent_dict["rec_face"] if model_emage.cfg.lf > 0 and model_emage.cfg.cf == 0 else None
         upper_latent = latent_dict["rec_upper"] if model_emage.cfg.lu > 0 and model_emage.cfg.cu == 0 else None
         hands_latent = latent_dict["rec_hands"] if model_emage.cfg.lh > 0 and model_emage.cfg.ch == 0 else None
         lower_latent = latent_dict["rec_lower"] if model_emage.cfg.ll > 0 and model_emage.cfg.cl == 0 else None
 
-        face_index = torch.max(F.log_softmax(latent_dict["cls_face"], dim=2), dim=2)[1] if model_emage.cfg.cf > 0 else None
+        face_index  = torch.max(F.log_softmax(latent_dict["cls_face"], dim=2), dim=2)[1] if model_emage.cfg.cf > 0 else None
         upper_index = torch.max(F.log_softmax(latent_dict["cls_upper"], dim=2), dim=2)[1] if model_emage.cfg.cu > 0 else None
         hands_index = torch.max(F.log_softmax(latent_dict["cls_hands"], dim=2), dim=2)[1] if model_emage.cfg.ch > 0 else None
         lower_index = torch.max(F.log_softmax(latent_dict["cls_lower"], dim=2), dim=2)[1] if model_emage.cfg.cl > 0 else None
@@ -105,8 +107,8 @@ def inference_emage(audio_path, sr_model, pose_fps):
     motion_pred = all_pred["motion_axis_angle"]
     t = motion_pred.shape[1]
     motion_pred = motion_pred.cpu().numpy().reshape(t, -1)
-    face_pred = all_pred["expression"].cpu().numpy().reshape(t, -1)
-    trans_pred = all_pred["trans"].cpu().numpy().reshape(t, -1)
+    face_pred   = all_pred["expression"].cpu().numpy().reshape(t, -1)
+    trans_pred  = all_pred["trans"].cpu().numpy().reshape(t, -1)
     npz_path = os.path.join(save_folder, "emage_output.npz")
     beat_format_save(npz_path, motion_pred, upsample=30 // pose_fps, expressions=face_pred, trans=trans_pred)
     return npz_path
@@ -137,13 +139,13 @@ def inference_app(audio, model_type, render_mesh=False, render_face=False, rende
         npz_path = inference_emage(tmp_audio_path, sr_model, pose_fps)
 
     motion_dict = np.load(npz_path, allow_pickle=True)
-    v2d_body = render2d(motion_dict, (720, 480), face_only=False, remove_global=True)
+    v2d_body    = render2d(motion_dict, (720, 480), face_only=False, remove_global=True)
     out_2d_body = npz_path.replace(".npz", "_2dbody.mp4")
     write_video(out_2d_body, v2d_body.permute(0, 2, 3, 1), fps=30)
     final_2d_body = out_2d_body.replace(".mp4", "_audio.mp4")
     fast_render.add_audio_to_video(out_2d_body, tmp_audio_path, final_2d_body)
 
-    final_mesh_video = None
+    final_mesh_video     = None
     final_meshface_video = None
     if render_mesh:
         mesh_vid = fast_render.render_one_sequence_no_gt(
@@ -159,7 +161,7 @@ def inference_app(audio, model_type, render_mesh=False, render_face=False, rende
 
     final_face_video = None
     if render_face:
-        v2d_face = render2d(motion_dict, (720, 480), face_only=True, remove_global=True)
+        v2d_face    = render2d(motion_dict, (720, 480), face_only=True, remove_global=True)
         out_2d_face = npz_path.replace(".npz", "_2dface.mp4")
         write_video(out_2d_face, v2d_face.permute(0, 2, 3, 1), fps=30)
         final_face_video = out_2d_face.replace(".mp4", "_audio.mp4")
